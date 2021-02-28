0fbd89bd59045b6d198fb28faa85951a1a97490b
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-02-28 12:35:22 +0700

    Add trainer notes, and implement view binding

--- app/src/main/java/com/enigmacamp/mycoroutine/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/mycoroutine/MainActivity.kt
@@ -4,24 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.enigmacamp.mycoroutine.databinding.ActivityMainBinding
 
+/*
+    - Di dalam pengembangan aplikasi android, jangan ada proses yang mem-blocking main thread
+
+    - Main thread dalam android, adalah thread yang befungsi untuk meng-update screen UI,
+      meng-handle event click, dan beberap UI callback
+
+    - Update screen UI oleh Main Thread dilakukan dengan kecepatan 60 frame per detik (dengan kata lain
+      proses update screen dilakukan per 16ms)
+
+    - Banyak proses yang kita pakai nanti, lebih lambat dari update screen UI, seperti nulis data
+      ke database, fetch data ke API, oleh karenanya apabila proses tersebut dilakukan di Main Thread
+      dapat menyebabkan UI pause/freeze, dan kalau lebih parah, waktu freeze nya lama, bisa menyebabkan
+      Application Not Responding
+
+    - Coba simulasikan contoh sederhana dibawah ini
+
+ */
 class MainActivity : AppCompatActivity() {
-    private lateinit var counterTextView: TextView
-    private lateinit var startButton: Button
-    private lateinit var stopButton: Button
+    private lateinit var binding: ActivityMainBinding
     var counterResult = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        counterTextView = findViewById(R.id.counter_textView)
-        startButton = findViewById(R.id.start_button)
-        stopButton = findViewById(R.id.stop_button)
-        startButton.setOnClickListener {
-            repeat(1_000) {
-                counterResult++
-                Thread.sleep(1500)
-                counterTextView.setText(counterResult.toString())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            counterTextView
+            startButton.setOnClickListener {
+                fakeHeavyProcessSimulation()
             }
+        }
+    }
+
+    private fun fakeHeavyProcessSimulation() {
+        repeat(1_000) {
+            counterResult++
+            Thread.sleep(1000)
+            binding.counterTextView.setText(counterResult.toString())
         }
     }
 }
